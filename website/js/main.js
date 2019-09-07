4e9f4a9d9fa86e6eb7d4fdba965623da5b8cc8b8
Abhishekucs <abhishekucskumar@gmail.com>
2019-09-08 00:34:07 +0530

    js update 2

--- website/js/main.js
+++ website/js/main.js
@@ -1,4 +1,6 @@
-if($('body').hasClass('mobile')) {
-    var viewportHeight = $('body').outerHeight();
-    $('body').css({height: viewportHeight});
-}
+let vh = window.innerHeight * 0.01;
+document.documentElement.style.setProperty('--vh','${vh}px');
+window.addEventListener('resize',() =>{
+    let vh = window.innerHeight * 0.01;
+    document.documentElement.style.setProperty('--vh','${vh}px');
+});
