f8e4aa117d5d0d00ca286abf31496f8482feb195
Abhishekucs <abhishekucskumar@gmail.com>
2019-09-08 01:08:42 +0530

    js update

--- website/js/main.js
+++ website/js/main.js
@@ -1,5 +1,5 @@
-let vh = window.innerHeight * 0.01;
-document.documentElement.style.setProperty('--vh','${vh}px');
+// let vh = window.innerHeight * 0.01;
+// document.documentElement.style.setProperty('--vh','${vh}px');
 window.addEventListener('resize',() =>{
     let vh = window.innerHeight * 0.01;
     document.documentElement.style.setProperty('--vh','${vh}px');
